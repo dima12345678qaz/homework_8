7d42b313baa494be531e7142dd0915c3ad2a57dc
dima12345678qaz <dmitrora93@gmail.com>
2022-04-26 10:24:46 +0200

    add home work

---
+++ script.js
@@ -0,0 +1,35 @@
+//tasl1
+
+// function isUpperCase(str) {
+//     const rage = /^[A-Z]/;
+// if (str.match(rage)) {
+//     return 'Strings starts with uppercase character';
+// } else {
+//     return 'Strings not starts with uppercase character ';
+
+// }
+// }
+// console.log(isUpperCase('Dfsfsdfsdf'));
+
+
+//task2
+// function checkEmail(str) {
+//         const rage = /^\S+@\S+\.\S+$/;
+//         return str.match(rage) ? true: false;
+//     }
+//     console.log(checkEmail('Email2@gmail.com'));
+    
+
+//task4
+
+// console.log('Java Script'.split(/ /).reverse().join(','));
+
+
+//task5;
+// function checkCard(str) {
+//         const rage = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/;
+//         return str.match(rage) ? true: false;
+//     }
+//     console.log(checkCard('9999-9999-9999-9999'));
+    
+
